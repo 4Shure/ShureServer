9cec0fe6b7971448e5ffac60a004ecbc0d4c129f
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-10-28 00:33:17 -0300

    Create utility file `utils.go`

---
+++ cmd/internal/utils/utils.go
@@ -0,0 +1,15 @@
+package utils
+
+import "time"
+
+func FormatEpoch(millis int64) string {
+	return time.UnixMilli(millis).
+		UTC().
+		Format(time.RFC3339)
+}
+
+func NowUTC() int64 {
+	return time.Now().
+		UTC().
+		UnixMilli()
+}
